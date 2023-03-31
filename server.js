8344a47439879aa7af8e325cb5b4d89c581bfe23
secretivo <secretivo@secretivos-Air.lan>
2023-03-30 21:52:43 -0400

    tip calculator

--- server.js
+++ server.js
@@ -3,27 +3,26 @@
 const express = require('express');
 const app = express();
 
-//-----------Route + Params --------------------------//
-//-----------Route + Params---------------------------//
-app.set('view engine', 'ejs');
-app.get('/greeting/:name', (req, res) => {
-  const name = req.params.name;
-  let message = '';
-  if (name === undefined) {
-    message = 'Hello, stranger';
-  } else if (name === 'X') {
-    message = 'Wow! Hello there, X';
-  } else if (name.startsWith('X')) {
-    message = `What's up, ${name}`;
-  } else {
-    message = `${name}! It's so great to see you!`;
-  }
-  res.render('greeting', { message });
-});
+//-----------Greeting--------------------------//
+//-----------Greeting--------------------------//
+// app.set('view engine', 'ejs');
+app.get("/greeting/:name", (req, res) => {
+  const name = req.params.name
+  res.send(`Hey there, ${req.params.name}`);
+})
+
+//-----------Tip Calculator------------------------//
+//-----------Tip Calculator------------------------//
+app.get("/tip/:total/:percentage", (req, res) =>{
+  const total = req.params.total
+  const tipPercent =  req.params.percentage/100
+  const tipAmount = total * tipPercent
+  res.send("Your tip amount is $" + tipAmount)
+  // console.log('total');
+})
+
 //-----------Listen for Server------------------------//
 //-----------Listen for Server------------------------//
-
-
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
